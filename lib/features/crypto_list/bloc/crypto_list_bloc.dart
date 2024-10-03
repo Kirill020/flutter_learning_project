97490cb8f9950b9a6fa61faca962b14388d52e3d
Kirill020 <work.tanasiichuk@gmail.com>
2024-10-03 17:14:41 +0200

    Added Equatable
    Added Equatable to check if we need to rebuild our widgets after refresh

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 
 import 'package:crypto_currencies_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'crypto_list_event.dart';
@@ -11,7 +12,9 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
   CryptoListBloc(this.coinsRepository) : super(CryptoListInitial()) {
     on<LoadCryptoList>((event, emit) async {
       try {
-        emit(CryptoListLoading());
+        if (state is! CryptoListLoaded) {
+          emit(CryptoListLoading());
+        }
         final cryptoCoinsList = await coinsRepository.getCoinsList();
         emit(CryptoListLoaded(coinsList: cryptoCoinsList));
       } catch (e) {
